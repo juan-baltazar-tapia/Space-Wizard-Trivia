53a19ee74cd55feac86069a205e7222211e53fbe
MrFxde <juan.space14@gmail.com>
2024-03-03 16:02:06 -0500

    basic layout

---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Card = (props) => {
+    return (
+        <>
+            <div className="container" >
+                <h2>{props.information}</h2>
+                <p></p>
+            </div>
+
+        </>
+    )
+}
+
+export default Card;
